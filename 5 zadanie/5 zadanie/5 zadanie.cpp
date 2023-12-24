58934f91a0f6939386c3d3a0485edb74427c5e2a
zrdagdelen <145365549+zrdagdelen@users.noreply.github.com>
2023-12-24 14:57:36 +0300

    Add files via upload

---
+++ 5 zadanie/5 zadanie/5 zadanie.cpp
@@ -0,0 +1,96 @@
+﻿#include <fstream>
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// Функция для нахождения максимальной площади прямоугольника в гистограмме
+int largestRectangleArea(vector<int>& heights) {
+    stack<int> s;
+    int maxArea = 0;
+    int tp;  // Индекс верхнего элемента стека
+    int areaWithTop;
+
+    int i = 0;
+    while (i < heights.size()) {
+        if (s.empty() || heights[s.top()] <= heights[i]) {
+            s.push(i++);
+        }
+        else {
+            tp = s.top();
+            s.pop();
+            areaWithTop = heights[tp] * (s.empty() ? i : i - s.top() - 1);
+
+            if (maxArea < areaWithTop) {
+                maxArea = areaWithTop;
+            }
+        }
+    }
+
+    while (!s.empty()) {
+        tp = s.top();
+        s.pop();
+        areaWithTop = heights[tp] * (s.empty() ? i : i - s.top() - 1);
+
+        if (maxArea < areaWithTop) {
+            maxArea = areaWithTop;
+        }
+    }
+
+    return maxArea;
+}
+
+int maximalRectangle(vector<vector<int>>& matrix) {
+    if (matrix.empty()) {
+        return 0;
+    }
+
+    int maxArea = 0;
+    vector<int> heights(matrix[0].size(), 0);
+
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; j < matrix[i].size(); ++j) {
+            if (matrix[i][j] == 0) {
+                heights[j] = 0;
+            }
+            else {
+                heights[j] += matrix[i][j];
+            }
+        }
+        maxArea = max(maxArea, largestRectangleArea(heights));
+    }
+
+    return maxArea;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    ifstream file("matrix.txt");// для txt файла (бинарный файл у меня не получилось создать)
+    //ifstream file("matrix.bin", ios::binary); // для бинарного файла
+    if (file) {
+        // Чтение двоичного файла и создание матрицы
+        vector<vector<int>> matrix;
+        char c;
+        vector<int> row;
+        while (file.get(c)) {
+            if (c == '0' || c == '1') {
+                row.push_back(c - '0');
+            }
+            if (c == '\n') {
+                matrix.push_back(row);
+                row.clear();
+            }
+        }
+
+        // Вызываем функцию для поиска максимальной прямоугольной области
+        int maxRectangleArea = maximalRectangle(matrix);
+
+        cout << "Максимальная площадь прямоугольника, содержащего только 1: " << maxRectangleArea << endl;
+    }
+    else {
+        cerr << "Не удалось открыть файл matrix.bin" << endl;
+        return 1;
+    }
+
+    return 0;
+}
---
+++ 6 zadanie/6 zadanie/6 zadanie.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int n = prices.size();
+    if (n <= 1) {
+        return 0;
+    }
+
+    int buy1 = INT_MIN, sell1 = 0;
+    int buy2 = INT_MIN, sell2 = 0;
+
+    for (int i = 0; i < n; i++) {
+        buy1 = max(buy1, -prices[i]);
+        sell1 = max(sell1, buy1 + prices[i]);
+        buy2 = max(buy2, sell1 - prices[i]);
+        sell2 = max(sell2, buy2 + prices[i]);
+    }
+
+    return sell2;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // Пример ввода массива цен акций от пользователя и передача его в функцию расчета прибыли
+    vector<int> prices;
+    int n;
+    cout << "Введите количество дней: ";
+    cin >> n;
+
+    cout << "Введите цены акций для каждого дня: ";
+    for (int i = 0; i < n; i++) {
+        int price;
+        cin >> price;
+        prices.push_back(price);
+    }
+
+    int result = maxProfit(prices);
+    cout << "Максимальная прибыль, которую можно получить: " << result << endl;
+
+    return 0;
+}
